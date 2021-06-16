13ccfdf9955b136949a7f52b62caef49275575f1
Christian Câmara de Abreu <christian@MacBook-Air-de-Christian.local>
2021-06-16 20:08:56 -0300

    video 100 8:18

--- src/App.js
+++ src/App.js
@@ -1,20 +1,49 @@
 import React from 'react'
-import {Text} from 'react-native'
+import {Text } from 'react-native'
 
 import {NavigationContainer} from '@react-navigation/native'
 import {createStackNavigator} from '@react-navigation/stack'
 import UserForm from './views/UserForm'
 import UserList from './views/UserList'
+import { BorderlessButton } from 'react-native-gesture-handler'
+
+import {Button, Icon} from 'react-native-elements'
 
 const Stack = createStackNavigator()
 
 export default props => {
     return (
        <NavigationContainer>
-           <Stack.Navigator initialRouteName="UserList" >
-               <Stack.Screen name="UserList" component={UserList} />
-               <Stack.Screen name="UserForm" component={UserForm} />
+           <Stack.Navigator initialRouteName="UserList" screenOptions={screenOptions} >
+               <Stack.Screen name="UserList" component={UserList} 
+               options={()=> 
+                    {
+                    return {
+                            title:'Lista de Usuários', 
+                            headerRight: ()=> 
+                            (
+                                <Button type="clear" title="Novo"
+                                icon={<Icon name="add" size={25} color="white" />}
+                                />    
+                            )
+                          }
+                    }
+                }
+               />
+               <Stack.Screen name="UserForm" component={UserForm}
+               options={{title:'Formulário de Usuários'}}
+               />
            </Stack.Navigator>
        </NavigationContainer>
     )
+}
+
+const screenOptions = {
+    headerStyle:{
+        backgroundColor: '#f4511e',
+    },
+    headerTintColor: '#FFFFFF',
+    headerTitleStyle: {
+        fontWeight: 'bold',
+    }
 }
