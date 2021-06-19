d648c71f1c72708e9119ead435dfc6fa0963c7ea
Christian Câmara de Abreu <christian@MacBook-Air-de-Christian.local>
2021-06-19 14:57:46 -0300

    lista de user mockada - traz email no subtitulo e clique abre tela de formulario - ainda sem nada

--- src/views/UserList.js
+++ src/views/UserList.js
@@ -4,15 +4,21 @@ import { ListItem, Avatar } from 'react-native-elements'
 import users from '../data/users'
 
 export default props => {
+    function editar(props){
+        props.navigation.navigate('UserForm')
+    }
+
     function getUserItem({item:user}){
-        return <ListItem>
+        return <ListItem bottomDivider onPress={ ()=> {editar(props)} }  >
         <Avatar
             title={user.name}
             source={{ uri: user.avatarUrl }}
         />
         <ListItem.Content>
             <ListItem.Title>{user.name}</ListItem.Title>
+            <ListItem.Subtitle>{user.email}</ListItem.Subtitle>
         </ListItem.Content>
+        <ListItem.Chevron/>
         </ListItem> ;       
         /*
         return <ListItem 
