47de66d57c0c306607c4af0dd8773dd70ea901a9
Christian Câmara de Abreu <christian@MacBook-Air-de-Christian.local>
2021-06-19 18:45:04 -0300

    form user ui traz dados e botao embaixo layout ok

--- src/views/UserForm.js
+++ src/views/UserForm.js
@@ -1,9 +1,29 @@
 import React, { useState } from 'react'
-import {Text} from 'react-native'
+import {Text, TextInput, View, StyleSheet, Button} from 'react-native'
 
 export default props => {
     const [user, setUser] = useState(props.route.params?props.route.params:{})
     return (
-        <Text>{user.name}</Text>
+        <View>
+            <Text>Nome</Text>
+            <TextInput style={styles.campo} onChangeText={name => setUser({...user,name})} placeholder="Informar Nome" value={user.name} />
+
+            <Text>Email</Text>
+            <TextInput style={styles.campo} onChangeText={email => setUser({...user,email})} placeholder="Informar Email" value={user.email} />
+
+            <Text>URL do Avatar</Text>
+            <TextInput style={styles.campo} onChangeText={avatarUrl => setUser({...user,avatarUrl})} placeholder="Informar URL" value={user.avatarUrl} />  
+
+            <Button title="Salvar" onPress={()=>{
+                props.navigation.goBack()
+            }
+            } />
+
+        </View>
     )
 }
+
+const styles = StyleSheet.create({
+    campo :{color:'blue', borderBottomColor:'black', borderBottomWidth: 1}
+
+})
