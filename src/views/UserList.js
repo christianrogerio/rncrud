f054220a5855e8aba29226a104ebff55581e28b2
Christian Câmara de Abreu <christian@MacBook-Air-de-Christian.local>
2021-06-19 15:47:05 -0300

    botao de edita e excluir

--- src/views/UserList.js
+++ src/views/UserList.js
@@ -1,24 +1,62 @@
 import React from 'react'
-import {Text, View, FlatList} from 'react-native'
-import { ListItem, Avatar } from 'react-native-elements'
+import {Text, View, FlatList, Alert} from 'react-native'
+import { ListItem, Avatar , Button, Icon} from 'react-native-elements'
 import users from '../data/users'
 
 export default props => {
+    function confirmUserDeletion(user){
+        Alert.alert('Excluir Usuário','Deseja excluir mesmo?',[
+            {text: 'Sim',onPress(){
+                console.warn('excluiu '+user.id)
+            }},
+            {text:'Não'}
+        ])
+    }
+
     function editar(props){
         props.navigation.navigate('UserForm')
     }
 
+    function getActions(user){
+        return (
+            <>
+                <Button type="clear" title="Editar"
+                                                onPress={()=>props.navigation.navigate('UserForm', user)}
+                                                icon={<Icon name="edit" size={25} color="blue" />}
+                                                />    
+            </>
+        )
+    }
+
     function getUserItem({item:user}){
-        return <ListItem bottomDivider onPress={ ()=> {editar(props)} }  >
+        return <ListItem  bottomDivider  >
         <Avatar
             title={user.name}
             source={{ uri: user.avatarUrl }}
         />
-        <ListItem.Content>
+        <ListItem.Content >
             <ListItem.Title>{user.name}</ListItem.Title>
             <ListItem.Subtitle>{user.email}</ListItem.Subtitle>
+
+            <ListItem.Chevron
+        name="edit"
+        size={25}
+        color="orange"
+        onPress={() => props.navigation.navigate('UserForm', user)}
+      />
+
+
+<ListItem.Chevron
+        name="delete"
+        size={25}
+        color="red"
+        onPress={() => confirmUserDeletion(user)}
+      />
+
         </ListItem.Content>
-        <ListItem.Chevron/>
+        
+
+        
         </ListItem> ;       
         /*
         return <ListItem 
