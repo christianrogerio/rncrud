7946905db36450ad464bc12c00fd074e63f07d22
Christian Câmara de Abreu <christian@MacBook-Air-de-Christian.local>
2021-06-19 13:24:33 -0300

    Lista de intens com aavtar de uma lista Mockup

--- src/views/UserList.js
+++ src/views/UserList.js
@@ -1,10 +1,25 @@
 import React from 'react'
 import {Text, View, FlatList} from 'react-native'
+import { ListItem, Avatar } from 'react-native-elements'
 import users from '../data/users'
 
 export default props => {
     function getUserItem({item:user}){
-        return <Text>{user.name} - {user.email}</Text>
+        return <ListItem>
+        <Avatar
+            title={user.name}
+            source={{ uri: user.avatarUrl }}
+        />
+        <ListItem.Content>
+            <ListItem.Title>{user.name}</ListItem.Title>
+        </ListItem.Content>
+        </ListItem> ;       
+        /*
+        return <ListItem 
+        leftAvatar={{source: {uri: user.avatarUrl, size:"small"}}}
+        />
+        */
+       // return <Text>{user.name} - {user.email}</Text>
     }
     return (
         <FlatList keyExtractor={user=> user.id.toString()} data={users} renderItem={getUserItem} />
